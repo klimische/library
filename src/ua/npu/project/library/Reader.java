0ca2ea02e98860dcd8b2c34f8efed4b0b8c7e84f
klimische <klimische@gmail.com>
2016-02-02 10:32:59 +0200

    Update Reader.java

--- src/ua/npu/project/library/Reader.java
+++ src/ua/npu/project/library/Reader.java
@@ -3,4 +3,9 @@ package ua.npu.project.library;
 //Читач
 
 public class Reader {
+  private String name; // имя 
+	private String Family; // фамилия
+	private String Adress; // место жительства
+	
+	
 }
